e00c50cc89841af76936b894bd51721b98e065cd
Igor Aleynikov <iharaleynikov@gmail.com>
2024-01-24 18:26:52 +0300

    src/cli: args.js, env.js functional

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,21 @@
+// node args.js --propName value --prop2Name value2
+
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  const propNameIndex = 0;
+  const prop2NameIndex = 2;
+
+  const result = [];
+
+  if (propNameIndex > -1 && prop2NameIndex > -1) {
+    result.push(
+      `${args[propNameIndex]} is ${args[propNameIndex + 1]}`,
+      `${args[prop2NameIndex]} is ${args[prop2NameIndex + 1]}`
+    );
+  }
+
+  console.log(result.join(', '));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
+// RSS_name1=value1 RSS_name2=value2 node env.js
+
 const parseEnv = () => {
-    // Write your code here 
+  // process.env.RSS_name1 = 'value1';
+  // process.env.RSS_name2 = 'value2';
+
+  const result = [];
+
+  for (const value in process.env) {
+    if (value.startsWith('RSS_')) {
+      result.push(`${value}=${process.env[value]}`);
+    }
+  }
+
+  console.log(result.join('; '));
 };
 
 parseEnv();
