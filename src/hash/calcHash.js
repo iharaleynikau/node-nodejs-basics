2dbe52383fbc4ebb5753e4c8e94838f3e8fed2c6
Igor Aleynikov <iharaleynikov@gmail.com>
2024-01-25 11:13:12 +0300

    hash/calcHash.js functional

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import { createHash } from 'node:crypto';
+import { createReadStream } from 'node:fs';
+
 const calculateHash = async () => {
-    // Write your code here 
+  const hash = createHash('sha256');
+
+  const stream = createReadStream('./files/fileToCalculateHashFor.txt');
+
+  stream.on('data', chunk => {
+    hash.update(chunk);
+  });
+
+  stream.on('end', () => {
+    console.log(hash.digest('hex'));
+  });
 };
 
 await calculateHash();
