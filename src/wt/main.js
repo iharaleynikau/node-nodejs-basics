39bb1e6fe98710a83b2c221002da95814c3d2f81
Igor Aleynikov <iharaleynikov@gmail.com>
2024-01-28 12:11:09 +0300

    src/wt: main.js, worker.js functional

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import { Worker, isMainThread, threadId, workerData } from 'node:worker_threads';
+import os from 'node:os';
+
 const performCalculations = async () => {
-    // Write your code here
+  const cpuCoresCount = os.cpus().length;
+
+  let startNumber = 10;
+
+  const resultColl = [];
+
+  for (let i = 1; i <= cpuCoresCount; i++) {
+    const worker = new Worker('./worker.js', {
+      workerData: startNumber++
+    });
+
+    worker.on('error', err => {
+      resultColl.push({
+        status: 'error',
+        data: null
+      });
+    });
+
+    worker.once('message', result => {
+      resultColl.push({
+        status: 'resolved',
+        data: result
+      });
+
+      if (resultColl.length === cpuCoresCount) {
+        console.log(resultColl);
+      }
+    });
+
+    worker.on('exit', () => {});
+  }
 };
 
 await performCalculations();
