d71ca326393d67d77e0a61ddd7d50edb1fbeea55
Igor Aleynikov <iharaleynikov@gmail.com>
2024-01-29 12:07:11 +0300

    cp.js functional

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,19 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import { spawn } from 'node:child_process';
+
+const spawnChildProcess = async args => {
+  const spawnChildProcess = spawn('node', ['./files/script.js', ...args], {
+    stdio: [process.stdin, process.stdout, process.stderr]
+  });
+
+  await new Promise((resolve, reject) => {
+    spawnChildProcess.on('exit', () => {
+      resolve();
+    });
+
+    spawnChildProcess.on('error', err => {
+      reject(err);
+    });
+  });
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['-s', '--arg1', '--arg2', '-E']);
